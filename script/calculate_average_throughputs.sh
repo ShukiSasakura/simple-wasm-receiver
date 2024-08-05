ec4a288fddd7e73975e0e21eae3801cf611dfbd5
ShukiSasakura <pbje8wfi@s.okayama-u.ac.jp>
2024-08-06 08:55:36 +0900

    Add script that calculate average of throughputs

---
+++ script/calculate_average_throughputs.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+TOP_DIR=$( dirname $0 )
+LOG_DIRS=$( echo "$1" | sed "s/\/$//" )
+
+directories=$( ls $LOG_DIRS/ )
+
+echo "throughput" > $LOG_DIRS/throughput_average.stat
+
+for i in $( seq 2 40 )
+do
+    sum=0
+
+    for logdir in $directories
+    do
+        throughput=$( sed -n "${i}p" "$LOG_DIRS/$logdir/throughput.stat" )
+        sum=$( echo "$sum + $throughput" | bc )
+    done
+
+    average=$( echo "scale=2; $sum / 5" | bc )
+    echo $average >> $LOG_DIRS/throughput_average.stat
+done
