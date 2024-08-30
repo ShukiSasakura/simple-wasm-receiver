f6a58c7b6e63b0bca0557e8d8625206ebe1cc36e
ShukiSasakura <pbje8wfi@s.okayama-u.ac.jp>
2024-08-30 16:00:07 +0900

    Change TOPDIR and LOGDIR

--- script/test_native.sh
+++ script/test_native.sh
@@ -1,26 +1,28 @@
 #!/usr/bin/env bash
 
-TOPDIR=$(dirname "$0")
-
 date=$(date +"%Y%m%d-%H%M%S")
 
-mkdir ../log/native/$date
+TOPDIR=$(dirname $(dirname "$0"))
+LOGDIR=$TOPDIR/log/native/$date
+
+mkdir -p $LOGDIR
 
 for TOTAL_N_SENDERS in $(seq 1 40)
 do
 
     killall receiver
 
-    num_messages=$(( 10000 / TOTAL_N_SENDERS ))
+    num_messages=$((10000 / TOTAL_N_SENDERS))
 
     # invoke receiver
-    $TOPDIR/../receiver/target/release/receiver& > ../log/native/$date/$date-$TOTAL_N_SENDERS-$num_messages.log
+    $TOPDIR/receiver/target/release/receiver \
+    > $LOGDIR/$date-$TOTAL_N_SENDERS-$num_messages.log&
     sleep 1
 
     # invoke sender
-    for i in $(seq 1 TOTAL_N_SENDERS)
+    for i in $(seq 1 $TOTAL_N_SENDERS)
     do
-        $TOPDIR/../sender/target/release/sender -m $num_messages&
+        $TOPDIR/sender/target/release/sender -m $num_messages&
     done
 
     sleep 4
--- script/test_wasmer.sh
+++ script/test_wasmer.sh
@@ -1,26 +1,28 @@
 #!/usr/bin/env bash
 
-TOPDIR=$(dirname "$0")
-
 date=$(date +"%Y%m%d-%H%M%S")
 
-mkdir ../log/wasm/$date
+TOPDIR=$(dirname $(dirname "$0"))
+LOGDIR=$TOPDIR/log/wasm/$date
+
+mkdir -p $LOGDIR
 
 for TOTAL_N_SENDERS in $(seq 1 40)
 do
 
     killall wasmer
 
-    num_messages=$(( 10000 / TOTAL_N_SENDERS ))
+    num_messages=$((10000 / TOTAL_N_SENDERS))
 
     # invoke receiver
-    wasmer --net $TOPDIR/../receiver/target/wasm32-wasmer-wasi/release/receiver.wasm& > ../log/wasm/$date/$date-$TOTAL_N_SENDERS-$num_messages.log
+    wasmer --net $TOPDIR/receiver/target/wasm32-wasmer-wasi/release/receiver.wasm \
+    > $LOGDIR/$date-$TOTAL_N_SENDERS-$num_messages.log&
     sleep 1
 
     # invoke sender
-    for i in $(seq 1 TOTAL_N_SENDERS)
+    for i in $(seq 1 $TOTAL_N_SENDERS)
     do
-        $TOPDIR/../sender/target/release/sender -m $num_messages&
+        $TOPDIR/sender/target/release/sender -m $num_messages&
     done
 
     sleep 4
