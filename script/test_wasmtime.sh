09a2be0ef72d6de08d21477d0c75b141f8033fc3
ShukiSasakura <pbje8wfi@s.okayama-u.ac.jp>
2024-10-11 17:49:16 +0900

    Fix receiver target name

--- script/test_wasmtime.sh
+++ script/test_wasmtime.sh
@@ -15,7 +15,7 @@ do
     num_messages=$((10000 / TOTAL_N_SENDERS))
 
     # invoke receiver
-    wasmtime -S tcplisten="127.0.0.1:8000" -S threads $TOPDIR/receiver/target/wasm32-wasip1/release/receiver.wasm \
+    wasmtime -S tcplisten="127.0.0.1:8000" -S threads $TOPDIR/receiver/target/wasm32-wasip1-threads/release/receiver.wasm \
     > $LOGDIR/$date-$TOTAL_N_SENDERS-$num_messages.log&
     sleep 1
 
